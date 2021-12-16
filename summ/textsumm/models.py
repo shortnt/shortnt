0ae6dc39c941cf3465a64abc557e68531da9c0c5
NaveedMohiuddin <78361672+NaveedMohiuddin@users.noreply.github.com>
2021-12-16 12:11:10 +0530

    Update models.py

--- summ/textsumm/models.py
+++ summ/textsumm/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class QuickSummary(models.Model):
     Id = models.AutoField(primary_key=True)
     Input = models.CharField(max_length=1000000)
-    Output = models.CharField(max_length=1000000)
+    Output = models.CharField(max_length=1000000,default="null")
 # Create your models here.
