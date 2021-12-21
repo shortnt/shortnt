136f5ab81c1ad2abb8dc062c744507cb3ec5daf3
Mohammed-Sameeruddin <65489551+Mohammed-Sameeruddin@users.noreply.github.com>
2021-12-21 22:13:08 +0530

    auth

--- summ/textsumm/models.py
+++ summ/textsumm/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
+from passlib.hash import pbkdf2_sha256
+
 class QuickSummary(models.Model):
     Id = models.AutoField(primary_key=True)
     Input = models.CharField(max_length=1000000)
     Output = models.CharField(max_length=1000000,default="null")
-# Create your models here.
+
+
+class SocialUserDetails(models.Model):
+    uid = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    image = models.CharField(max_length=1000)
+
+
+class ManualUserDetails(models.Model):
+    mid = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=1000)
+    image = models.CharField(max_length=1000,default="null")
