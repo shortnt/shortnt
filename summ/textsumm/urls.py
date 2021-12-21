0815d2475c27178573721c2ecc8860f021ab028d
Mohammed-Sameeruddin <65489551+Mohammed-Sameeruddin@users.noreply.github.com>
2021-12-21 22:14:54 +0530

    auth

--- summ/textsumm/urls.py
+++ summ/textsumm/urls.py
@@ -7,5 +7,10 @@ from django.conf import settings
 urlpatterns=[
     re_path(r'^quicksummary/$',views.quickSummaryApi),
     re_path(r'^quicksummary/([0-9]+)$',views.quickSummaryApi),
+    re_path(r'^socialuser/$',views.userDetailsApi),
+    re_path(r'^socialuser/([0-9]+)$',views.userDetailsApi),
+    re_path(r'^manualuser/$',views.manualDetailsApi),
+    re_path(r'^manualuser/([0-9]+)$',views.manualDetailsApi),
+    re_path(r'^loginuser/$',views.loginCred),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
