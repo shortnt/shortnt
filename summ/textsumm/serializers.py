f0897d8ea182957c2abebf6d09b61cf6042dee7d
Mohammed-Sameeruddin <65489551+Mohammed-Sameeruddin@users.noreply.github.com>
2021-12-21 22:13:47 +0530

    auth

--- summ/textsumm/serializers.py
+++ summ/textsumm/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
-from textsumm.models import QuickSummary
+from textsumm.models import QuickSummary,SocialUserDetails,ManualUserDetails
 
 class QuickSerializer(serializers.ModelSerializer):
     class Meta:
         model = QuickSummary
-        fields = ('Id',
-                  'Input',
-                  'Output')
+        fields = ('Id','Input','Output')
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SocialUserDetails
+        fields = ('uid','username','email','image')
+
+
+class ManualUserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ManualUserDetails
+        fields = ('mid','username','email','password','image')
